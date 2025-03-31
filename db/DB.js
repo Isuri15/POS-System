dab82af0ba00648ca48d8559025e9bbba2f2b7ec
Isuri15 <isurigamage13@gmail.com>
2025-03-31 12:33:03 +0530

    first commit

---
+++ db/DB.js
@@ -0,0 +1,33 @@
+export let Customers = [
+  {
+    custId: "C001",
+    custName: "Isuri",
+    custAddress: "Matara",
+    custSalary: "80000",
+  },
+
+  {
+    custId: "C002",
+    custName: "Samadi",
+    custAddress: "Panadura",
+    custSalary: "75000",
+  },
+];
+
+export let Items = [
+  {
+      itemId: "I001",
+      itemName: "Chocolate",
+      itemQty: 25,
+      itemPrice: 250,
+  },
+  {
+    itemId: "I002",
+    itemName: "Toffee",
+    itemQty: 15,
+    itemPrice: 10,
+  },
+  
+];
+
+export let Orders = [];
